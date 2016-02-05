9b798dac1345c0bb86d83ba3c5511342f86de1ca
inspiringinovation <vasincaalex@gmail.com>
2016-02-05 11:07:26 +0200

    initial Splashscreen implementation

--- app/src/main/java/com/example/alin/memoxvp/SplashActivity.java
+++ app/src/main/java/com/example/alin/memoxvp/SplashActivity.java
@@ -16,6 +16,7 @@ public class SplashActivity extends AppCompatActivity{
             @Override
             public void run() {
                 try{
+
                     sleep(1000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
