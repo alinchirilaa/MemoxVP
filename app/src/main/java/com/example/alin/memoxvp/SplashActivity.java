5f4449c7ca90865ab4a99e3f2b19a6df9ff21b71
inspiringinovation <vasincaalex@gmail.com>
2016-02-05 11:01:47 +0200

    initial Splashscreen implementation

---
+++ app/src/main/java/com/example/alin/memoxvp/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.alin.memoxvp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vasin on 2/5/2016.
+ */
+public class SplashActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        new Thread (){
+            @Override
+            public void run() {
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        }.start();
+
+
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+}
