97e5c353ee08647b425cf1c209fda7ee62880ef5
Alex Vasinca <vasincaalex@gmail.com>
2016-02-10 00:37:20 +0200

    + description is now in dialog + website now opens in chrome custom tab

--- app/src/main/java/com/example/alin/memoxvp/About.java
+++ app/src/main/java/com/example/alin/memoxvp/About.java
@@ -1,24 +1,30 @@
 package com.example.alin.memoxvp;
 
-import android.content.Context;
-import android.graphics.Color;
+
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.ViewCompat;
+import android.content.Intent;
+import android.support.customtabs.CustomTabsCallback;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
-import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
+import android.widget.Button;
+import android.graphics.BitmapFactory;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class About extends AppCompatActivity {
+
+    private final String url = "http://www.memox.ro/";
+
+    private CustomTabsClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,67 +33,92 @@ public class About extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeButtonEnabled(true);   //Back button to home enable
-        actionBar.setDisplayHomeAsUpEnabled(true);   //  -//-
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-//                                      --WebView code--
-
-        String summary = "<html><body><p align=\"justify\"><b>Aplicatia MemoX VP</b> a fost conceputa pentru a usura activitatea " +
-                "persoanelor care activeaza in domeniul financiar contabil si nu numai. Scopul acesteia este ca sa reduca timpul" +
-                " petrecut pe site-urile institutiilor publice pentru verificarea clientilor si furnizorilor unei organizatii in " +
-                "vederea obtinerii de informatii cu privire la:  datele de identificare, status (activ/inactiv, insolventa)," +
-                " inregistrarea TVA / TVAI, cod VIES. Cum in practica pot interveni destul de frecvent modificari ale datelor " +
-                "agentilor economici, un astfel de instrument reprezinta o necesitate.<br>In acest scop oferim organizatiilor " +
-                "posibilitatea de a-si verifica in timp real si cu o economie de timp considerabila intreaga baza de date de clienti " +
-                "si furnizori cu care acestea colaboreaza.<br>Sursele de date utilizate sunt: Ministerul de Finante (MF)," +
-                "Agentia Nationala de Administrare Fiscala (ANAF), Oficiul National al Registrului Comertului (ONRC) si Baze de date proprii." +
-                "Se verifica datele inregistrate la Ministerul de Finante (datele de identificare, TVA, TVAI)," +
-                "in Registrul contribuabililor inactivi/reactivati, in Registrul persoanelor impozabile care aplica sistemul de TVA la incasare," +
-                "Buletinul Insolventei, iar pentru partenerii externi se valideaza numarul de inregistrare pentru TVA-VIES.</p></body></html>";
-
-        WebView webView = (WebView) findViewById(R.id.webView1);
-        webView.loadData(summary, "text/html", null);
-        webView.setBackgroundColor(Color.TRANSPARENT);
-        webView.getSettings().setBuiltInZoomControls(true);   //zoom_in zoom_out working
-        webView.getSettings().setDisplayZoomControls(false);  //hide zoom control
+        Button moreDetailsButton = (Button) findViewById(R.id.button_about);
+        moreDetailsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
 
 
-//                                      --WebView code--
 
-        //--------------------------------------------------------------------------------------//
+                AlertDialog.Builder builder = new AlertDialog.Builder(About.this ,R.style.AppCompatAlertDialogStyle);
+                builder.setTitle(R.string.info_title);
+                builder.setMessage(R.string.info_description);
+                builder.setPositiveButton(R.string.ok,null);
+                builder.show();
+            }
+
+        });
+
+        CustomTabsServiceConnection mConnection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
+                mClient = customTabsClient;
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                    mClient = null;
+            }
+        };
+
+        String packageName = "com.android.chrome";
+        CustomTabsClient.bindCustomTabsService(this,packageName,mConnection);
 
 
-//        TextView myTextview;
-//        myTextview= (TextView) findViewById(R.id.txtAbout);
-//        myTextview.setText(Html.fromHtml("<html><body><p align=\"justify\"><b>Aplicatia MemoX VP</b> a fost conceputa pentru a usura activitatea " +
-//                "persoanelor care activeaza in domeniul financiar contabil si nu numai. Scopul acesteia este ca sa reduca timpul" +
-//                "petrecut pe site-urile institutiilor publice pentru verificarea clientilor si furnizorilor unei organizatii in" +
-//                "vederea obtinerii de informatii cu privire la:  datele de identificare, status (activ/inactiv, insolventa)," +
-//                "inregistrarea TVA / TVAI, cod VIES. Cum in practica pot interveni destul de frecvent modificari ale datelor " +
-//                "agentilor economici, un astfel de instrument reprezinta o necesitate.<br>In acest scop oferim organizatiilor " +
-//                "posibilitatea de a-si verifica in timp real si cu o economie de timp considerabila intreaga baza de date de clienti" +
-//                "si furnizori cu care acestea colaboreaza.<br>Sursele de date utilizate sunt: Ministerul de Finante (MF)," +
-//                "Agentia Nationala de Administrare Fiscala (ANAF), Oficiul National al Registrului Comertului (ONRC) si Baze de date proprii." +
-//                "Se verifica datele inregistrate la Ministerul de Finante (datele de identificare, TVA, TVAI)," +
-//                "in Registrul contribuabililor inactivi/reactivati, in Registrul persoanelor impozabile care aplica sistemul de TVA la incasare," +
-//                "Buletinul Insolventei, iar pentru partenerii externi se valideaza numarul de inregistrare pentru TVA-VIES.</p></body></html>"));
-//        myTextview.setMovementMethod(new ScrollingMovementMethod());
+        TextView textViewSRL = (TextView) findViewById(R.id.about_srl);
+        TextView textViewNr1 = (TextView) findViewById(R.id.about_nr1);
+        TextView textViewNr2 = (TextView) findViewById(R.id.about_nr2);
+        TextView textViewAdresa = (TextView) findViewById(R.id.about_adresa);
 
 
-                                //-------FAB dispare din ABout-------
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-                                //-------FAB dispare din ABout-------
 
 
     }
+
+    public void prefetchContent(View view){
+        if (mClient != null) {
+            mClient.warmup(0);
+            CustomTabsSession customTabsSession = getSession();
+            customTabsSession.mayLaunchUrl(Uri.parse(url),null,null);
+
+        }
+    }
+
+    public CustomTabsSession getSession(){
+        return mClient.newSession(new CustomTabsCallback(){
+            @Override
+            public void onNavigationEvent(int navigationEvent, Bundle extras) {
+                super.onNavigationEvent(navigationEvent, extras);
+            }
+        });
+    }
+
+    public void loadCustomTabs(View view){
+        CustomTabsIntent.Builder mBuilder = new CustomTabsIntent.Builder(getSession());
+        mBuilder.setToolbarColor(getResources().getColor(R.color.colorPrimary));
+        mBuilder.setCloseButtonIcon(BitmapFactory.decodeResource(getResources(),
+                R.mipmap.ic_arrow_back_white_24dp));
+        mBuilder.addMenuItem("Share", setMenuItem());
+        mBuilder.setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left);
+        mBuilder.setExitAnimations(this, R.anim.slide_in_left, R.anim.slide_out_right);
+        CustomTabsIntent mIntent = mBuilder.build();
+        mIntent.launchUrl(this, Uri.parse(url));
+
+    }
+
+    private PendingIntent setMenuItem() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Official website of Anjith Sasindran");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+        return PendingIntent.getActivity(this, 0, shareIntent, 0);
+    }
+
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
